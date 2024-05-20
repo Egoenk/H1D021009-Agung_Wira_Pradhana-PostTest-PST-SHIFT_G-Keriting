e535c41eab70ed29c4998cea245ec760860fa4ee
Egy <neoegoenk@gmail.com>
2024-05-20 16:10:11 +0700

    Mengurangi LED

--- main.cpp
+++ main.cpp
@@ -5,17 +5,14 @@ int myFunction(int, int);
 
 int trigPin = D1;
 int echoPin = D0;
-int led = D3;
 Servo myservo;
 long duration ; 
 int distance ;
 
 void setup() {
-  pinMode(led,OUTPUT);
   pinMode(trigPin,OUTPUT);
   pinMode(echoPin, INPUT);
   myservo.attach(D4);
-
 }
 
 void loop() {
@@ -47,7 +44,6 @@ int myFunction(int x, int y) {
 Penjelasan 
 
 Setup : 
-pinMode(led, OUTPUT);: Mengatur pin led (D3).
 pinMode(trigPin, OUTPUT);: Mengatur pin trigPin (D1).
 pinMode(echoPin, INPUT);: Mengatur pin echoPin (D0).
 myservo.attach(D4);: Mengatur servo ke pin D4.
@@ -63,7 +59,8 @@ Distance = 0.034 * duration / 2;: Menghitung jarak berdasarkan durasi sinyal ech
 if (distance <= 50) { 
     myservo.write(90); 
     } else { myservo.write(0); 
-    }: Mengatur posisi servo berdasarkan jarak yang diukur. Jika jarak kurang dari atau sama dengan 50 cm, servo diatur ke posisi 90 derajat. Jika tidak, servo diatur ke posisi 0 derajat.
+    }: 
+    Mengatur posisi servo berdasarkan jarak yang diukur. Jika jarak kurang dari atau sama dengan 50 cm, servo diatur ke posisi 90 derajat. Jika tidak, servo diatur ke posisi 0 derajat.
 delay(1000);: Menunggu 1 detik sebelum mengulangi loop.
 
 */
